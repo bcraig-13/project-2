d4416b9dc1f06652ad71aca0c4e6bc12f8a8c971
Elijah Davis <elijahgdavis1999@gmail.com>
2021-01-31 16:57:44 -0800

    Fixed static html and added routes and hrefs

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/all-pokemon");
     }
     res.render("signup");
   });
@@ -13,14 +13,18 @@ module.exports = function(app) {
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/all-pokemon");
     }
     res.render("login");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.render("members", { username: req.user.username });
+  app.get("/all-pokemon", isAuthenticated, (req, res) => {
+    res.render("all-pokemon", { username: req.user.username });
+  });
+
+  app.get("/add-pokemon", isAuthenticated, (req, res) => {
+    res.render("add-pokemon", { username: req.user.username });
   });
 };
