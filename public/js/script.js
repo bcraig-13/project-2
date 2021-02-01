79ed5a4ed7c845c31aa1beba093ec50a7a87f91a
Brandon Craig <thegamingraven13@gmail.com>
2021-01-31 23:36:33 -0800

    Completed search function for pokemon api. Need to correct the id's to their corresponding id's on the html.

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,18 @@
 $(document).ready(() => {
-  const Pokedex = require("pokedex-promise-v2");
-  const P = new Pokedex();
-  const pkmnSearch = $("#SEARCH");
-  const addTo = $("#ADDTO");
+  function pokeSubmit() {
+    let param = document.getElementById("searchVal").value;
+    let pokeURL = "http://pokeapi.co/api/v2/pokemon/" + param.toLowerCase();
 
-  pkmnSearch.on("click", (event) => {
-    event.preventDefault();
-    let searchVal = $("#SEARCH").val();
-  });
+    $.getJSON(pokeURL, function(data) {
+      let type = data.types;
+      $("#sprite").html(`<img src="${data.sprites.front_default}">`);
+      $("#pkmn-name").html(data.name);
+      if (type.length === 2) {
+        $("#pkmn-type1").html(type[0].type.name);
+        $("#pkmn-type2").html(type[1].type.name);
+      } else {
+        $("#pkmn-type1").html(type[0].type.name);
+      }
+    });
+  }
 });
