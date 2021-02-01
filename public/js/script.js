d433c1c565b8c85330589f430d9e0cfe4ffa4240
Brandon Craig <thegamingraven13@gmail.com>
2021-02-01 12:24:22 -0800

    add-pokemon now works properly. Made few spelling and aesthetic changes to add-pokemon.handlebars.

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,31 @@
-$(document).ready(() => {
-  function pokeSearch() {
-    const param = document.getElementById("#searchVal").value;
-    const pokeURL = "http://pokeapi.co/api/v2/pokemon/" + param.toLowerCase();
-    //Each # needs a matching id on the html
-    $.getJSON(pokeURL, data => {
-      const type = data.types;
-      $(".card-img-top").html(`<img src="${data.sprites.front_default}">`);
-      $("#pkmnName").html(data.name);
-      if (type.length === 2) {
-        $("#typeOne").html(type[0].type.name);
-        $("#typeTwo").html(type[1].type.name);
-      } else {
-        $("#typeOne").html(type[0].type.name);
-      }
-    });
-  }
+function pokeSearch() {
+  const param = $("#searchVal").val();
+  const pokeURL = "http://pokeapi.co/api/v2/pokemon/" + param.toLowerCase();
+  //Each # needs a matching id on the html
 
-  function pokeSubmit() {
-    if ($("#pkmnName") === null || $("levelVal" === null)) {
-      message.textContent =
-        "Search for a pokemon and enter an integer in the level input.";
+  $.getJSON(pokeURL, data => {
+    const type = data.types;
+    console.log(data);
+    $(".card-img-top").attr("src", data.sprites.front_default);
+    $(".card-img-top").attr("alt", data.name);
+    $("#pkmnName").html(data.name);
+    if (type.length === 2) {
+      $("#typeOne").html(type[0].type.name);
+      $("#typeTwo").html(type[1].type.name);
     } else {
-      // Insert card data into db
+      $("#typeOne").html(type[0].type.name);
     }
-  }
+  });
+}
 
-  $("#search").on("click", pokeSearch());
-  $("#submit").on("click", pokeSubmit());
-});
+function pokeSubmit() {
+  if ($("#pkmnName") === null || $("levelVal" === null)) {
+    message.textContent =
+      "Search for a pokemon and enter an integer in the level input.";
+  } else {
+    // Insert card data into db
+  }
+}
+
+$("#search").on("click", pokeSearch);
+// $("#submit").on("click", pokeSubmit());
